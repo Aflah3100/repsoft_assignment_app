fdb2e6c40ad6058d73e4d2e79eb63e89da2734b3
Aflah Sedhique <aflah3100@gmail.com>
2024-09-13 17:31:05 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:repsoft_assignment_app/screens/sign_in_screen/sign_in_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Repsoft Assignment App',
+      theme: ThemeData(
+      
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xffFF6B17)),
+        useMaterial3: true,
+      ),
+      home: SignInScreen(),
+    );
+  }
+}
